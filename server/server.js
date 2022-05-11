b2ad25209b78cac381b1a4c7e5bb1604d98620ea
james chandler <80052602+jameschandler1@users.noreply.github.com>
2022-05-11 13:06:27 -0700

    fixed it

--- server/server.js
+++ server/server.js
@@ -1,33 +1,36 @@
 const express = require('express');
 const models = require('./models');
-const expressGraphQL = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
-if (!MONGO_URI) {
+const db_uri = process.env.MONGO_URI;
+if (!db_uri) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(db_uri);
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
 
-app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
+app.use(express.json());
+app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
 
-const webpackMiddleware = require('webpack-dev-middleware');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
-app.use(webpackMiddleware(webpack(webpackConfig)));
+const config = webpack(require('../webpack.config.js'));
+app.use(webpackDevMiddleware(config));
+
 
 module.exports = app;
